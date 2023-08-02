bbf293a4f34e060974b63165f6db84973b8be59c
Jasvaran Mann <mannjesse355@gmail.com>
2023-08-02 15:37:23 -0700

    created model for messages

---
+++ models/messages.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const { DateTime } = require('luxon')
+
+const MessageSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+})
+
+
+MessageSchema.virtual("date").get(function() {
+    return DateTime.fromJSDate(this.timestamp).toFormat("yyyy-MM-dd, HH:mm")
+})
+
+module.exports = mongoose.model("Message", MessageSchema)
